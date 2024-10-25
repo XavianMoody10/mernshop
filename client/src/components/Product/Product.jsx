a37d2d541927b7f0faa9ccf9f74a7999ff1aff4a
Xavian Moody <XavianMoody1025@gmail.com>
2024-10-25 13:26:30 -0400

    Added animation to products

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 export const Product = ({ info }) => {
+  const [isProductVisible, setIsProductVisible] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const ref = useRef(null);
+
+  function intersectionObserveProduct() {
+    const options = { threshold: 0.2 };
+
+    function callback(entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsProductVisible(true);
+        }
+      });
+    }
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(ref.current);
+  }
+
+  useEffect(() => {
+    intersectionObserveProduct();
+  }, []);
+
   return (
-    <div className=" space-y-3">
+    <motion.div
+      className=" space-y-3 min-h-[609.656px]"
+      ref={ref}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isProductVisible ? 1 : 0 }}
+    >
       <div>
-        <img src={info.images[0].baseUrl} alt={`${info.name} image`} />
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isProductVisible && isImageLoaded ? 1 : 0 }}
+          transition={{ ease: "easeIn", delay: 1 }}
+          src={info.images[0].baseUrl}
+          alt={`${info.name} image`}
+          onLoad={() => setIsImageLoaded(true)}
+        />
       </div>
+
+      {!isImageLoaded && <div className=" min-h-[460px]"></div>}
+
       <p className=" text-lg font-semibold text-center">{info.name}</p>
       <p className=" text-xl text-center">${info.price.value}</p>
       <button className=" border w-full py-4 shadow-sm hover:shadow-md duration-150 font-semibold">
         Add To Cart
       </button>
-    </div>
+    </motion.div>
   );
 };
