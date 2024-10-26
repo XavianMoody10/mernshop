344dcd1a054239b1f85c7f95ea1f93bb13463b82
Xavian Moody <XavianMoody1025@gmail.com>
2024-10-26 18:47:20 -0400

    Deploy to vercel has succeded

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,14 +11,14 @@ async function enableMocking() {
   const { worker } = await import("./mocks/browser");
 
   // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept request
+  // once the Service Worker is up and ready to intercept requests.
   return worker.start({ onUnhandledRequest: "bypass" });
 }
 
-// enableMocking().then(() => {
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-// });
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
