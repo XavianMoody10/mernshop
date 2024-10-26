2e6c6eb3d620dc79eb9c047fe1ab5f847445b1e0
Xavian Moody <XavianMoody1025@gmail.com>
2024-10-26 19:19:49 -0400

    Update for vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 async function enableMocking() {
-  if (import.meta.env.PROD) {
+  if (process.env.NODE_ENV !== "development") {
     return;
   }
 
