23a894f1f90358d0dc328913ee5a7643637e17a2
Xavian Moody <XavianMoody1025@gmail.com>
2024-10-25 23:59:35 -0400

    Added zindex to header

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -6,7 +6,7 @@ import logo from "../assets/logo.png";
 
 export const Header = () => {
   return (
-    <header className=" sticky top-0 bg-white bg-opacity-35 py-8 lg:py-10">
+    <header className=" sticky top-0 bg-white bg-opacity-35 py-8 z-50 lg:py-10">
       <div className=" flex items-center justify-between w-[90%] max-w-[1700px] mx-auto">
         <Link to={"/"}>
           <div className=" max-w-[180px]">
